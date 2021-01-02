203c6dd4e0b603f20d338614bf109dd121c5a95a
Abdul <a-baghdadi@outlook.com>
2021-01-02 21:22:18 +0100

    get_goals validate
    TODO: and & special moves (spX)

--- Megaminx/megaminx-rotations.pl
+++ Megaminx/megaminx-rotations.pl
@@ -1,29 +1,5 @@
 %---------------------------------------------------%
-%       DEBUGGING NOTES!
-%
-% 1) Muss die angeschaute Seite nicht auch gedreht werden? z.B.: bei move(h) -> h werte rotieren (Mani,Mo)
-%       -> H0,H1,H2,H3,H4,H5,H6,H7,H8,H9,H10, %5
-%       -> H0,H9,H10,H1,H2,H3,H4,H5,H6,H7,H8, %5
-%   Begruendung: Wenn noch nicht alle Seiten richtig sind werden "illegale" verdrehungen durchgefuehrt
-%                Bei uns sind die Variablen nur Platzhalter und nicht die Farben direkt! 
-%
-%
-%
-%
-%
-%
-%---------------------------------------------------%
-
-
-
-
-
 % the actual rotations are perfomed here
-
-
-
-
-
 % Simulation Mov 2
 mov(w,
 megaminx(
@@ -323,7 +299,7 @@ Pr0,Pr1,Pr2,Pr3,Pr4,Pr5,Pr6,Pr7,Pr8,Pr9,Pr10, %3
 T0,T1,T2,T3,T4,T5,T6,T7,T8,T9,T10, %4
 H0,H1,H2,H3,H4,H5,H6,H7,H8,H9,H10, %5
 R0,R1,R2,R3,R4,R5,R6,R7,R8,R9,R10, %6
-L0,L9,L10,L1,L2,L3,L4,L5,L6,L7,L8, %7 
+L0,L9,L10,L1,L2,L3,L4,L5,L6,L7,L8, %7
 %************************************
 Gr0,Gr1,Gr2,Gr3,Gr4,O3,O4,O5,Gr8,Gr9,Gr10, %8
 %-------------------********-----------------
@@ -492,10 +468,318 @@ Pi0,Pi9,Pi10,Pi1,Pi2,Pi3,Pi4,Pi5,Pi6,Pi7,Pi8 %12
 %***********************************************
 )).
 
-
+% --------------------------------------------------------------
+% some special "moves" that are mapped to longer move sequences
+% --------------------------------------------------------------
+mov(sp1,X,Y) :-
+move_sequence([], X, Y).
+  %move_sequence([-f, +d, +f, -d, -l, -d, +l], X, Y).
+mov(sp2,X,Y) :-
+  move_sequence([+f, -d, -f, +d, -r, +d, +r], X, Y).
+mov(sp3,X,Y) :-
+  move_sequence([-b, +r, -d, -r, +d, +b], X, Y).
+mov(sp4,X,Y) :-
+  move_sequence([-b, -d, +r, +d, -r, +b], X, Y).
+mov(sp5,X,Y) :-
+  move_sequence([-b, +r, -d, -r, +d, +b, +f, -d, -l, +d, +l, -f], X, Y).
+mov(sp6,X,Y) :-
+  move_sequence([-l, -l, -u, -d, -d, +l, +r, +f, +f, -l, -r, +u, -l, -l], X, Y).
+mov(sp7,X,Y) :-
+  move_sequence([-l, -l, -u, +l, +r, +f, +f, -l, -r, -d, -d, +u, -l, -l], X, Y).
+mov(sp8,X,Y) :-
+  move_sequence([-l, -l, +u, +u, -b, -b, +u, +r, +r, +f, +f, +r, +r, +f, +f, +r, +r, +f, +f, -u, -b, -b, +u, +u, -l, -l], X, Y).
+mov(sp9,X,Y) :-
+  move_sequence([-r, -d, -l, +d, +r, -d, +l, +d], X, Y).
+mov(sp10,X,Y) :-
+  move_sequence([-d, -l, +d, -r, -d, +l, +d, +r], X, Y).
+mov(sp11,X,Y) :-
+  move_sequence([+l, -b, -b, +f, -l, -f, +l, +f, -l, -f, +l, +f, -l, -f, +l, -b, -b, -l], X, Y).
+mov(sp12,X,Y) :-
+  move_sequence([-b, +r, -d, -r, +d, +r, -d, -r, +d, +r, -d, -r, +d, +b], X, Y).
+mov(sp13,X,Y) :-
+  move_sequence([+f, +u, -f, -u, +f, +u, -f, -u], X, Y).
+mov(sp14,X,Y) :-
+  move_sequence([+u, +f, -u, -f, +u, +f, -u, -f], X, Y).
+% --------------------------------------------------------------
+% these helpers call the actual rotations
+% --------------------------------------------------------------
 % clockwise move
 move(+Move, OldState, NewState) :-
-mov(Move, OldState, NewState).
+  mov(Move, OldState, NewState).
 % counterclockwise move
 move(-Move, OldState, NewState) :-
-mov(Move, NewState, OldState).
+  mov(Move, NewState, OldState).
+% empty move list
+move_sequence([],X,X).
+% longer sequence
+move_sequence([Move|Othermoves], X, Z) :-
+  move(Move,X,Y),
+  move_sequence(Othermoves,Y,Z).
+% ----------------------------------------------------------------------
+% strategy: solve the cube in stages
+% stages 1-4: put the upper slice edge pieces (two-colored) in place
+% stages 5-8: put the upper slice corner pieces (three-colored) in place
+% stage 9: turn the cube so that the 'F' face center and the upper
+% slice 'F' face pieces are on the same (front) face
+% stages 10-20: put the middle slice corner pieces (two-colored) in place
+% stages 21-24: form a one-color "cross" on the 'D' face with bottom slice
+% (two-colored) edge pieces
+% stages 25-27: put the bottom slice edge pieces (two-colored) in place
+% stage 28: put the bottom slice corner pieces in place
+%
+%
+% here are the cube goal states for each stage
+% ----------------------------------------------------------------------
+
+% not checked
+get_goal(1, megaminx(
+_,_,_,_,_,_,_,_,_,_,_, %1
+"W","W",_,"W",_,"W",_,"W",_,"W",_, %2
+_,_,_,_,_,_,_,_,_,_,_, %3
+_,_,_,_,_,_,_,_,_,_,_, %4
+_,_,_,_,_,_,_,_,_,_,_, %5
+"R",_,_,_,_,"R",_,_,_,_,_, %6
+_,_,_,_,_,_,_,_,_,_,_, %7
+_,_,_,_,_,_,_,_,_,_,_, %8
+_,_,_,_,_,_,_,_,_,_,_, %9
+"Y",_,_,_,_,_,"Y",_,_,_,_, %10
+"B",_,_,_,_,_,"B",_,_,_,_, %11
+_,_,_,_,_,_,_,_,_,_,_) %12
+).
+
+get_goal(2,megaminx(
+_,_,_,_,_,_,_,_,_,_,_, %1
+"W","W","W","W","W","W","W","W","W","W","W", %2
+_,_,_,_,_,_,_,_,_,_,_, %3
+_,_,_,_,_,_,_,_,_,_,_, %4
+_,_,_,_,_,_,_,_,_,_,_, %5
+"R",_,_,_,"R","R","R",_,_,_,_, %6
+_,_,_,_,_,_,_,_,_,_,_, %7
+_,_,_,_,_,_,_,_,_,_,_, %8
+_,_,_,_,_,_,_,_,_,_,_, %9
+"Y",_,_,_,_,"Y","Y","Y",_,_,_,
+"B",_,_,_,_,"B","B","B",_,_,_,
+_,_,_,_,_,_,_,_,_,_,_)
+).
+
+get_goal(3,megaminx(
+_,_,_,_,_,_,_,_,_,_,_,%1
+"W","W","W","W","W","W","W","W","W","W","W", %2
+_,_,_,_,_,_,_,_,_,_,_, %3
+_,_,_,_,_,_,_,_,_,_,_, %4
+_,_,_,_,_,_,_,_,_,_,_, %5
+"R",_,_,"R","R","R","R","R",_,_,_, %6
+_,_,_,_,_,_,_,_,_,_,_, %7
+_,_,_,_,_,_,_,_,_,_,_, %8
+_,_,_,_,_,_,_,_,_,_,_, %9
+"Y",_,_,_,"Y","Y","Y","Y","Y",_,_,
+"B",_,_,_,"B","B","B","B","B",_,_,
+_,_,_,_,_,_,_,_,_,_,_)
+).
+
+get_goal(4,megaminx(
+_,_,_,_,_,_,_,_,_,_,_, %1
+"W","W","W","W","W","W","W","W","W","W","W", %2
+_,_,_,_,_,_,_,_,_,_,_, %3
+_,_,_,_,_,_,_,_,_,_,_, %4
+"H",_,_,_,_,"H","H","H",_,_,_, %5
+"R","R","R","R","R","R","R","R","R","R",_,  %6
+"L",_,_,_,_,"L","L","L",_,_,_, %7
+"Gr",_,_,_,_,_,_,_,_,_,_, %8
+_,_,_,_,_,_,_,_,_,_,_, %9
+"Y",_,_,_,"Y","Y","Y","Y","Y",_,_,
+"B",_,"B","B","B","B","B","B","B","B","B",
+"Pi",_,_,_,"Pi","Pi","Pi",_,_,_,_)
+).
+
+
+get_goal(5,megaminx(
+_,_,_,_,_,_,_,_,_,_,_, %1
+"W","W","W","W","W","W","W","W","W","W","W", %2
+_,_,_,_,_,_,_,_,_,_,_, %3
+_,_,_,_,_,_,_,_,_,_,_, %4
+"H",_,_,_,"H","H","H","H","H",_,_, %5
+"R","R","R","R","R","R","R","R","R","R","R",  %6
+"L",_,_,_,"L","L","L","L","L",_,_, %7
+"Gr",_,_,_,_,_,_,_,_,_,_, %8
+_,_,_,_,_,_,_,_,_,_,_, %9
+"Y",_,_,_,"Y","Y","Y","Y","Y",_,_,
+"B","B","B","B","B","B","B","B","B","B","B",
+"Pi",_,_,"Pi","Pi","Pi","Pi","Pi",_,_,_)
+).
+
+get_goal(6,megaminx(
+_,_,_,_,_,_,_,_,_,_,_, %1
+"W","W","W","W","W","W","W","W","W","W","W", %2
+_,_,_,_,_,_,_,_,_,_,_, %3
+_,_,_,_,_,_,_,_,_,_,_, %4
+"H",_,_,"H","H","H","H","H","H","H",_, %5
+"R","R","R","R","R","R","R","R","R","R","R",  %6
+"L",_,_,"L","L","L","L","L","L","L",_, %7
+"Gr",_,_,_,_,_,_,_,_,_,_, %8
+_,_,_,_,_,_,_,_,_,_,_, %9
+"Y",_,_,_,"Y","Y","Y","Y","Y",_,_,
+"B","B","B","B","B","B","B","B","B","B","B",
+"Pi",_,"Pi","Pi","Pi","Pi","Pi","Pi","Pi",_,_)
+).
+
+get_goal(7,megaminx(
+_,_,_,_,_,_,_,_,_,_,_, %1
+"W","W","W","W","W","W","W","W","W","W","W", %2
+_,_,_,_,_,_,_,_,_,_,_, %3
+_,_,_,_,_,_,_,_,_,_,_, %4
+"H",_,_,"H","H","H","H","H","H","H",_, %5
+"R","R","R","R","R","R","R","R","R","R","R",  %6
+"L",_,_,"L","L","L","L","L","L","L",_, %7
+"Gr",_,"Gr",_,"Gr",_,"Gr",_,"Gr",_,"Gr", %8
+_,_,_,_,_,_,_,_,_,_,_, %9
+"Y",_,_,_,"Y","Y","Y","Y","Y",_,_,
+"B","B","B","B","B","B","B","B","B","B","B",
+"Pi",_,"Pi","Pi","Pi","Pi","Pi","Pi","Pi",_,_)
+).
+
+get_goal(8,megaminx(
+_,_,_,_,_,_,_,_,_,_,_, %1
+"W","W","W","W","W","W","W","W","W","W","W", %2
+_,_,_,_,_,_,_,_,_,_,_, %3
+_,_,_,_,_,_,_,_,_,_,_, %4
+"H","H",_,"H","H","H","H","H","H","H",_, %5
+"R","R","R","R","R","R","R","R","R","R","R",  %6
+"L","L",_,"L","L","L","L","L","L","L",_, %7
+"Gr",_,"Gr",_,"Gr",_,"Gr",_,"Gr",_,"Gr", %8
+_,_,_,_,_,_,_,_,_,_,_, %9
+"Y",_,_,_,"Y","Y","Y","Y","Y",_,_,
+"B","B","B","B","B","B","B","B","B","B","B",
+"Pi","Pi","Pi","Pi","Pi","Pi","Pi","Pi","Pi",_,"Pi")
+).
+
+get_goal(9,megaminx(
+_,_,_,_,_,_,_,_,_,_,_, %1
+"W","W","W","W","W","W","W","W","W","W","W", %2
+_,_,_,_,_,_,_,_,_,_,_, %3
+_,_,_,_,_,_,_,_,_,_,_, %4
+"H","H",_,"H","H","H","H","H","H","H",_, %5
+"R","R","R","R","R","R","R","R","R","R","R",  %6
+"L","L",_,"L","L","L","L","L","L","L",_, %7
+"Gr","Gr","Gr","Gr","Gr","Gr","Gr","Gr","Gr","Gr","Gr", %8
+_,_,_,_,_,_,_,_,_,_,_, %9
+"Y",_,_,_,"Y","Y","Y","Y","Y",_,_,
+"B","B","B","B","B","B","B","B","B","B","B",
+"Pi","Pi","Pi","Pi","Pi","Pi","Pi","Pi","Pi",_,"Pi")
+).
+
+get_goal(10,megaminx(
+_,_,_,_,_,_,_,_,_,_,_, %1
+"W","W","W","W","W","W","W","W","W","W","W", %2
+_,_,_,_,_,_,_,_,_,_,_, %3
+_,_,_,_,_,_,_,_,_,_,_, %4
+"H","H","H","H","H","H","H","H","H","H","H", %5
+"R","R","R","R","R","R","R","R","R","R","R",  %6
+"L","L","L","L","L","L","L","L","L","L","L", %7
+"Gr","Gr","Gr","Gr","Gr","Gr","Gr","Gr","Gr","Gr","Gr", %8
+_,_,_,_,_,_,_,_,_,_,_, %9
+"Y",_,_,_,"Y","Y","Y","Y","Y",_,_,
+"B","B","B","B","B","B","B","B","B","B","B",
+"Pi","Pi","Pi","Pi","Pi","Pi","Pi","Pi","Pi","Pi","Pi")
+).
+
+% --------------------------------------------------------------
+% the search space is narrowed by listing all allowed rotations
+% for each stage separately
+% --------------------------------------------------------------
+and(1, [g, w, pr, t, h, r, pi, l, gr, o, y, b]).
+and(2, [g, w, pr, t, h, r, pi, l, gr, o, y, b]).
+and(3, [g, w, pr, t, h, r, pi, l, gr, o, y, b]).
+and(4, [g, w, pr, t, h, r, pi, l, gr, o, y, b]).
+and(5, [g, w, pr, t, h, r, pi, l, gr, o, y, b]).
+and(6, [g, w, pr, t, h, r, pi, l, gr, o, y, b]).
+and(7, [g, w, pr, t, h, r, pi, l, gr, o, y, b]).
+and(8, [g, w, pr, t, h, r, pi, l, gr, o, y, b]).
+and(9, [g, w, pr, t, h, r, pi, l, gr, o, y, b]).
+and(10, [g, w, pr, t, h, r, pi, l, gr, o, y, b]).
+
+
+%and(2, [r, d, f, t]).
+%and(3, [r, d, f, t]).
+%and(4, [r, d, f, t]).
+%and(5, [f, r, d, t]).
+%and(6, [r, d, f, t]).
+%and(7, [r, d, f, t]).
+%and(8, [r, d, f, t]).
+%and(9, [u, t]).
+%and(10, [f, d, r, sp1, sp2]).
+%and(11, [sp1, sp2]).
+%and(12, [t]).
+%and(13, [f, d, r, sp1, sp2]).
+%and(14, [sp1,sp2]).
+%and(15, [t]).
+%and(16, [f, d, r, sp1, sp2]).
+%and(17, [sp1,sp2]).
+%and(18, [t]).
+%and(19, [f, d, r, sp1, sp2]).
+%and(20, [sp1,sp2]).
+%and(21, [t, sp3, sp4, sp5]).
+%and(22, [t, sp3, sp4, sp5]).
+%and(23, [t, sp3, sp4, sp5]).
+%and(24, [t, sp3, sp4, sp5]).
+%and(25, [t, u, sp6, sp7, sp8]).
+%and(26, [t, u, sp6, sp7, sp8]).
+%and(27, [t, u, sp6, sp7, sp8]).
+%and(28, [t, u, sp9, sp10, sp11, sp12, sp13, sp14]).
+% catch a candidate move from the list for each stage
+get_candidate(Stage, Move) :-
+  and(Stage, Movelist),
+  nth1(_,Movelist,Move).
+% --------------------------------------------------------------
+% these helpers reject some unwise move candidates
+% check whether the proposed move should be tried or not
+% --------------------------------------------------------------
+% first, if this move is negation of previous move, we reject it
+conflict([+Previous|_], -This) :- This=Previous.
+conflict([-Previous|_], +This) :- This=Previous.
+% also, maximum moves in the direction is 2 (cw) or 1 (ccw)
+% (other can be reached with rotation to the other direction)
+conflict([-Previous|_], -This) :- This=Previous.
+conflict([+Previous|[+Previous|_]],+This) :- This=Previous.
+% ---------------------------------------------------------------
+% rotate performs the BFS search from given cube state to get the
+% the solution (a move list) for the defined stage.
+% New moves are proposed by get_move (cw and ccw directions).
+% ---------------------------------------------------------------
+% the list is empty, does not change the cube
+rotate(_,[],State,State).
+% Moves collects the answer move list (in REVERSE order).
+% State is the current state of the cube
+% Criteria contains the list of the pieces to be positioned in this stage
+rotate(Stage, Moves, State, Stagegoal) :-
+  rotate(Stage, Priormoves, State, Nextstate),
+  get_move(Stage, Nextmove, Nextstate, Stagegoal),
+  not(conflict(Priormoves,Nextmove)),
+  append([Nextmove], Priormoves, Moves).
+get_move(Stage, +Move, State, Stagegoal) :-
+  get_candidate(Stage, Move),
+  mov(Move, State, Stagegoal).
+get_move(Stage, -Move, State, Stagegoal) :-
+  get_candidate(Stage, Move),
+  mov(Move, Stagegoal, State).
+% --------------------------------------------------------------
+% Here are the topmost predicates that collect the answer
+% --------------------------------------------------------------
+% updates the stage
+get_stage(Stage, Newstage) :-
+  Newstage is Stage + 1.
+% the cube should be finished after stage 28, so we stop here.
+stage([],11,_).
+% this builds the result move list by moving from stage to stage
+% and joining the solutions gathered for each stage
+stage(Movelist,Currentstage,Cubestate) :-
+  get_goal(Currentstage,Targetstate),
+  rotate(Currentstage,Stagemoves,Cubestate,Targetstate),
+  reverse(Stagemoves,Newmoves),
+  move_sequence(Newmoves,Cubestate,Newstate),
+  get_stage(Currentstage,Newstage),
+  stage(Newlist,Newstage,Newstate),
+  append(Newmoves,Newlist,Movelist).
+% a little UI helper
+solve(Movelist,Cube_state) :-
+  stage(Movelist, 1, Cube_state).
